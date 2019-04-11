7a5f7f4ec0c3fc25fccf545a0da107752f2fc12b
moon-314 <49510386+moon-314@users.noreply.github.com>
2019-04-11 19:43:43 +0900

    JavaScript
    JavaScript

---
+++ JavaScript/로그인/로그인 1.js
@@ -0,0 +1,13 @@
+
+if(id=='lottegiant') {
+    password=prompt('비밀번호 입력');
+    if(password==='lottegiant') {
+        location.href="main.html" 
+    }
+    else {
+        location.href="main.html" 
+    }
+}
+else {
+    location.href="main.html" 
+}
